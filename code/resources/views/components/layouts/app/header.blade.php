593e794e3d18107b8f749a09041bdbbba2b4ab59
stijn-rombouts <r1036855@student.thomasmore.be>
2025-10-02 11:28:10 +0200

    Fix user UI

--- code/resources/views/components/layouts/app/header.blade.php
+++ code/resources/views/components/layouts/app/header.blade.php
@@ -52,6 +52,7 @@
 
                 <flux:menu>
                     <flux:menu.radio.group>
+                        <h1>ABC</h1>
                         <div class="p-0 text-sm font-normal">
                             <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
                                 <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
--- code/resources/views/components/layouts/app/sidebar.blade.php
+++ code/resources/views/components/layouts/app/sidebar.blade.php
@@ -32,10 +32,15 @@
             <!-- Desktop User Menu -->
             <flux:dropdown class="hidden lg:block" position="bottom" align="start">
                 <!-- <flux:profile
-                    :name="auth()->user() ? auth()->user()->name : ''"
-                    :initials="auth()->user() ? auth()->user()->initials() : ''"
+                    :name="auth()->user() ? auth()->firstname()->name : ''"
+                    :initials="auth()->user() ? auth()->user()->lastname() : ''"
                     icon:trailing="chevrons-up-down"
                 /> -->
+                <flux:profile
+                    :name="'name'"
+                    :initials="'ZZ'"
+                    icon:trailing="chevrons-up-down"
+                />
 
                 <flux:menu class="w-[220px]">
                     <flux:menu.radio.group>
@@ -45,13 +50,16 @@
                                     <span
                                         class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
                                     >
-                                        <!-- {{ auth()->user() ? auth()->user()->initials() : '' }} -->
+                                    ABC
+                                        <!-- {{ auth()->user() ? auth()->user()->firstname() : '' }} -->
                                     </span>
                                 </span>
 
                                 <div class="grid flex-1 text-start text-sm leading-tight">
                                     <!-- <span class="truncate font-semibold">{{ auth()->user() ? auth()->user()->name : '' }}</span>
                                     <span class="truncate text-xs">{{ auth()->user() ? auth()->user()->email : '' }}</span> -->
+                                    <span class="truncate font-semibold">Z</span>
+                                    <span class="truncate text-xs">X</span>
                                 </div>
                             </div>
                         </div>
@@ -59,11 +67,11 @@
 
                     <flux:menu.separator />
 
-                    <flux:menu.radio.group>
+                    <!-- <flux:menu.radio.group>
                         <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
                     </flux:menu.radio.group>
 
-                    <flux:menu.separator />
+                    <flux:menu.separator /> -->
 
                     <form method="POST" action="{{ route('logout') }}" class="w-full">
                         @csrf
@@ -82,26 +90,26 @@
             <flux:spacer />
 
             <flux:dropdown position="top" align="end">
-                <!-- <flux:profile
-                    :initials="auth()->user() ? auth()->user()->initials() : ''"
+                <flux:profile
+                    :initials="auth()->user() ? auth()->user()->firstname() : ''"
                     icon-trailing="chevron-down"
-                /> -->
+                />
 
                 <flux:menu>
                     <flux:menu.radio.group>
                         <div class="p-0 text-sm font-normal">
                             <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
                                 <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
-                                    <!-- <span
+                                    <span
                                         class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
                                     >
                                         {{ auth()->user() ? auth()->user()->initials() : '' }}
-                                    </span> -->
+                                    </span>
                                 </span>
 
                                 <div class="grid flex-1 text-start text-sm leading-tight">
-                                    <!-- <span class="truncate font-semibold">{{ auth()->user() ? auth()->user()->name : '' }}</span>
-                                    <span class="truncate text-xs">{{ auth()->user() ? auth()->user()->email : '' }}</span> -->
+                                    <span class="truncate font-semibold">{{ auth()->user() ? auth()->user()->name : '' }}</span>
+                                    <span class="truncate text-xs">{{ auth()->user() ? auth()->user()->email : '' }}</span>
                                 </div>
                             </div>
                         </div>
