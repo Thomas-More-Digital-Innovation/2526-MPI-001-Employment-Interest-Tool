376677c22b91964373cd892b46e2db2a17393387
stijn-rombouts <r1036855@student.thomasmore.be>
2025-10-02 14:39:06 +0200

    Fix auth

--- code/app/Models/User.php
+++ code/app/Models/User.php
@@ -14,6 +14,13 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
+        /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'user_id';
+
     /**
      * The attributes that are mass assignable.
      *
