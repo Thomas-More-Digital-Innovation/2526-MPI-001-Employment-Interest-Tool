d9c7a92166259ccced20cd7c03b0a0da89a6c447
David Maat <r1036789@student.thomasmore.be>
2025-10-03 11:14:00 +0200

    TestPage Tests

---
+++ code/database/factories/InterestFieldFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\InterestField;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class InterestFieldFactory extends Factory
+{
+    protected $model = InterestField::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->words(2, true),
+            'description' => $this->faker->sentence(),
+        ];
+    }
+
+    /**
+     * Create an interest field with a specific ID
+     */
+    public function withId(int $id): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'interest_field_id' => $id,
+        ]);
+    }
+}
