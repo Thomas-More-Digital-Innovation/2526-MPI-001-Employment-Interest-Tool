798890db769e19758002211caab4ac40d82f7285
David Maat <r1036789@student.thomasmore.be>
2025-10-03 15:52:59 +0200

    wip

--- code/app/Livewire/Test.php
+++ code/app/Livewire/Test.php
@@ -2,13 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Answer;
 use App\Models\Question;
+use App\Models\TestAttempt;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Test extends Component
 {
 
     public $testId = 1;
+    public $userId = 4;
+    public $testAttemptId;
+
+    public $startTime;
+
+    public $question;
     public $title;
     public $image;
     public $imageDescription;
@@ -28,6 +37,14 @@ class Test extends Component
         $this->totalQuestions = Question::where('test_id', $this->testId)->count();
         $this->testName = \App\Models\Test::where('test_id', $this->testId)->value('test_name');
 
+        if (!$this->testAttemptId) {
+            $this->testAttemptId = TestAttempt::create([
+                'test_id' => $this->testId,
+                'user_id' => $this->userId,
+            ])->test_attempt_id;
+        }
+
+        $this->newQuestion();
     }
 
     public function render()
@@ -45,28 +62,31 @@ class Test extends Component
         return view('livewire.test')->layout('components.layouts.test');
     }
 
-    public function close()
+    private function close()
     {
        return redirect()->route('dashboard');
     }
 
-    public function like()
+    private function like()
     {
+        $this->answer(true, null);
         $this->nextQuestion();
     }
 
-    public function dislike()
+    private function dislike()
     {
+        $this->answer(false, null);
+
         $this->nextQuestion();
     }
 
-    public function previous()
+    private function previous()
     {
 
         $this->previousQuestion();
     }
 
-    public function next()
+    private function next()
     {
         $this->nextQuestion();
     }
@@ -80,6 +100,7 @@ class Test extends Component
 
         $this->questionNumber++;
         $this->previousEnabled = true;
+        $this->newQuestion();
 
     }
 
@@ -91,5 +112,38 @@ class Test extends Component
 
         $this->questionNumber--;
         $this->previousEnabled = false;
+        $this->newQuestion();
     }
+
+    private function unclear()
+    {
+
+    }
+
+    private function newQuestion()
+    {
+        $this->startTime = Carbon::now();
+    }
+
+    private function answer($answer, $unclear)
+    {
+        $now = Carbon::now();
+
+        $question = Question::where('test_id', $this->testId)
+            ->where('question_number', $this->questionNumber)
+            ->firstOrFail();
+
+        Answer::updateOrCreate(
+            [
+                'test_attempt_id' => $this->testAttemptId,
+                'question_id'     => $question->question_id,
+            ],
+            [
+                'answer' => $answer,
+                'unclear' => $unclear,
+                'response_time' => $this->startTime->diffInSeconds($now)
+            ]
+        );
+    }
+
 }
