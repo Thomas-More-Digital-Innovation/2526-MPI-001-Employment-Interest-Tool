b561ae93e025264319ecca7c4d74cd80d5d18f36
stijn-rombouts <r1036855@student.thomasmore.be>
2025-10-04 15:22:16 +0200

    forgot and reset password styling

--- code/resources/views/components/auth-session-status.blade.php
+++ code/resources/views/components/auth-session-status.blade.php
@@ -3,7 +3,7 @@
 ])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
+    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600 bg-white border border-gray-200 rounded-lg px-4 py-3']) }}>
         {{ $status }}
     </div>
 @endif
