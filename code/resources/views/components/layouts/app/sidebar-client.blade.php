22480b7be7a03d9070bcc17abb9088d8f47d0af6
stijn-rombouts <r1036855@student.thomasmore.be>
2025-10-04 21:19:25 +0200

    Fix translations

--- code/resources/views/components/layouts/app/sidebar-client.blade.php
+++ code/resources/views/components/layouts/app/sidebar-client.blade.php
@@ -5,7 +5,7 @@
 
     <!-- Client specific navigation -->
     <flux:navlist.group :heading="__('Client')" class="grid">
-        <flux:navlist.item icon="home" :href="route('client.example')" :current="request()->routeIs('client.example')" wire:navigate>{{ __('general.take_test') }}</flux:navlist.item>
+        <flux:navlist.item icon="home" :href="route('client.example')" :current="request()->routeIs('client.example')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
         <flux:navlist.item icon="pencil" :href="route('client.taketest')" :current="request()->routeIs('client.taketest')" wire:navigate>{{ __('general.take_test') }}</flux:navlist.item>
     </flux:navlist.group>
 </flux:navlist>
--- code/resources/views/roles/client/taketest.blade.php
+++ code/resources/views/roles/client/taketest.blade.php
@@ -3,7 +3,7 @@
         <div class="space-y-6 p-6">
             <div class="bg-white dark:bg-gray-700 overflow-hidden shadow rounded-2xl">
                 <div class="px-4 py-5 sm:px-6">
-                    <h2 class="text-2xl font-bold text-gray-900 dark:text-white mb-4">{{ __('Take test') }}</h2>
+                    <h2 class="text-2xl font-bold text-gray-900 dark:text-white mb-4">{{ __('general.take_test') }}</h2>
                     @livewire('client-test-picker')
                 </div>
             </div>
