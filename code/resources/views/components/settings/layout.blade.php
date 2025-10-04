c8cb8a52df678057c6a6eece3c484d7259b25efc
Stijn Rombouts <r1036855@student.thomasmore.be>
2025-10-04 15:54:47 +0200

    Profile > Settings > Navigation > Show which item is selected (#51)

--- code/resources/views/components/settings/layout.blade.php
+++ code/resources/views/components/settings/layout.blade.php
@@ -1,9 +1,9 @@
 <div class="flex items-start max-md:flex-col">
     <div class="me-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>
-            <flux:navlist.item :href="route('settings.profile')" wire:navigate>{{ __('Profile') }}</flux:navlist.item>
-            <flux:navlist.item :href="route('settings.password')" wire:navigate>{{ __('Password') }}</flux:navlist.item>
-            <flux:navlist.item :href="route('settings.appearance')" wire:navigate>{{ __('System') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.profile')" wire:navigate :current="request()->routeIs('settings.profile')">{{ __('Profile') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.password')" wire:navigate :current="request()->routeIs('settings.password')">{{ __('Password') }}</flux:navlist.item>
+            <flux:navlist.item :href="route('settings.appearance')" wire:navigate :current="request()->routeIs('settings.appearance')">{{ __('System') }}</flux:navlist.item>
         </flux:navlist>
     </div>
 
