bdfb8dad2ff3f6806045ee6fd3f3b89c9519ceef
Stijn Rombouts <r1036855@student.thomasmore.be>
2025-10-06 20:15:54 +0200

    Move to new dashboard router (#62)

--- code/resources/views/components/layouts/app/sidebar-admin.blade.php
+++ code/resources/views/components/layouts/app/sidebar-admin.blade.php
@@ -1,10 +1,10 @@
 <flux:navlist variant="outline">
     <flux:navlist.group :heading="__('Platform')" class="grid">
-        <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+        <flux:navlist.item icon="home" :href="route('admin.dashboard')" :current="request()->routeIs('admin.dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
     </flux:navlist.group>
 
     <!-- Admin specific navigation -->
     <flux:navlist.group :heading="__('Admin Specific SideBar')" class="grid">
-        <flux:navlist.item icon="home" :href="route('admin.example')" :current="request()->routeIs('admin.example')" wire:navigate>{{ __('Example') }}</flux:navlist.item>   
+        <flux:navlist.item icon="home" :href="route('admin.example')" :current="request()->routeIs('admin.example')" wire:navigate>{{ __('Example') }}</flux:navlist.item>
     </flux:navlist.group>
 </flux:navlist>
