de6d3c66a664dd76ad9f09adf243bf0cb441e90b
StefanBotnariUni <r1033310@student.thomasmore.be>
2025-10-06 20:25:29 +0200

    updated layout for all settings pages, added a return to dashboard button

--- code/app/Livewire/Settings/Appearance.php
+++ code/app/Livewire/Settings/Appearance.php
@@ -3,8 +3,21 @@
 namespace App\Livewire\Settings;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Appearance extends Component
 {
-    //
+    /**
+     * Render the component.
+     */
+    public function render()
+    {
+        $user = Auth::user();
+
+        if ($user->isClient()) {
+            return view('livewire.settings.appearance')->layout('components.layouts.app.headerAIT');
+        }
+
+        return view('livewire.settings.appearance')->layout('components.layouts.app');
+    }
 }
