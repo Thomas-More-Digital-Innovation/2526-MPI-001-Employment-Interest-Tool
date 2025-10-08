8e93d7b3cd7e580b1c9f7cb08299e19201bd91fd
MauritsGroen <r1037355@student.thomasmore.be>
2025-10-08 13:50:25 +0200

    Working on researcher dashboard

--- code/app/Http/Controllers/DashboardController.php
+++ code/app/Http/Controllers/DashboardController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Role;
+use App\Models\User;
+use App\Models\Test;
+use App\Models\Answer;
+use App\Models\TestAttempt;
+use App\Models\InterestField;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use PHPUnit\TextUI\XmlConfiguration\IntroduceCacheDirectoryAttribute;
 
 class DashboardController extends Controller
 {
@@ -15,6 +22,20 @@ class DashboardController extends Controller
     {
         $user = Auth::user();
 
+        $totalUsers = User::count();
+        $totalTests = Test::count();
+        $interestFields = InterestField::all();
+        $mostChosenIntrestFields = DB::table('answer')
+            ->join('test_attempt', 'answer.test_attempt_id', '=', 'test_attempt.test_attempt_id')
+            ->join('question', 'answer.question_id', "=", "question.question_id")
+            ->join('interest_field', 'question.interest_field_id', "=", "interest_field.interest_field_id")
+            ->select(
+                'interest_field.name as interest_field_name',
+                DB::raw('COUNT(answer.answer_id) as total_chosen'))
+            ->groupBy('interest_field.interest_field_id', 'interest_field.name')
+            ->orderByDesc('total_chosen')
+            ->get();
+
         if ($user->isSuperAdmin()) {
             return view('roles.superadmin.dashboard');
         } elseif ($user->isAdmin()) {
@@ -24,7 +45,7 @@ class DashboardController extends Controller
         } elseif ($user->isClient()) {
             return view('roles.client.dashboard');
         } elseif($user->isResearcher()){
-            return view('roles.researcher.dashboard');
+            return view('roles.researcher.dashboard', compact('totalUsers', 'totalTests', 'interestFields', 'mostChosenIntrestFields'));
         }
 
         // Fallback for users without roles
--- code/resources/views/roles/researcher/dashboard.blade.php
+++ code/resources/views/roles/researcher/dashboard.blade.php
@@ -3,7 +3,34 @@
         <div class="space-y-6 p-6">
             <div class="bg-white overflow-hidden shadow rounded-lg">
                 <div class="px-4 py-5 sm:px-6">
-                    <h2 class="text-2xl font-bold tracking-tight text-gray-900">{{ __('Researcher Dashboard') }}</h2>
+                    <h2 class="text-2xl font-bold tracking-tight text-gray-900">{{ __('researcher.dashboardResearcher') }}</h2>
+                </div>
+            </div>
+            <div class="flex flex-wrap bg-white text-black rounded-lg px-4 py-5">
+                <div class="w-1/2 px-2">
+                    <h2 class="text-xl font-bold">Total of users</h2>
+                    <p>{{$totalUsers}}</p>
+                </div>
+                <div class="w-1/2 px-2">
+                    <h2 class="text-xl font-bold">Total of Tests</h2>
+                    <p>{{$totalTests}}</p>
+                </div>
+            </div>
+            <div class="flex flex-wrap bg-white text-black rounded-lg px-4 py-5">
+                <h2 class="text-center w-full text-xl font-bold">Times Intrestfield chosen</h2>
+                <div class="sm:w-full h-auto xl:w-3/4 xl:px-2">
+                    <livewire:chart
+                        :labels="['A','B','C', 'D', 'E', 'F', 'A','B','C', 'D', 'E', 'F']"
+                        :data="[100,2,3,1,100,2558,100,2,3,1,100,2558]"
+                        class="max-w-1"
+                    />
+                </div>
+                <div class="sm:w-full xl:w-1/4 xl:px-2 mt-10">
+                    <ol class="list-decimal list-inside ms-3">
+                        @foreach($mostChosenIntrestFields as $field)
+                            <li>{{$field->interest_field_name}} : {{$field->total_chosen}}</li>
+                        @endforeach
+                    </ol>
                 </div>
             </div>
         </div>
