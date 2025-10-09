57ad016644eb9a3d2c6350c8ef91d0e1d543c09d
MauritsGroen <r1037355@student.thomasmore.be>
2025-10-09 11:22:46 +0200

    update unit test

--- code/tests/Unit/DashboardGlobalOverviewTest.php
+++ code/tests/Unit/DashboardGlobalOverviewTest.php
@@ -8,16 +8,18 @@ use Tests\TestCase;
 
 class DashboardGlobalOverviewTest extends TestCase
 {
-    public function test_render_returns_view_with_variables()
+    public function test_view_receives_expected_variables()
     {
         $component = new DashboardGlobalOverview();
         $view = $component->render();
 
+        // Controleer dat render een view object teruggeeft
         $this->assertInstanceOf(View::class, $view);
 
-        $this->assertEquals('livewire.dashboard-global-overview', $view->getName());
-
+        // Verkrijg alle data uit de view
         $data = $view->getData();
+
+        // Controleer dat de verwachte variabelen bestaan
         $this->assertArrayHasKey('totalOrganisations', $data);
         $this->assertArrayHasKey('totalUsers', $data);
         $this->assertArrayHasKey('totalTests', $data);
