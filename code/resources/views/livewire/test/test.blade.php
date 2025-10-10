f54247a3bc94b8338587c67200bba2ce006bef68
David Maat <r1036789@student.thomasmore.be>
2025-10-10 14:51:48 +0200

    Added exit confirmation to test and fixed height layout bug on test page

--- code/resources/views/livewire/test/test.blade.php
+++ code/resources/views/livewire/test/test.blade.php
@@ -4,26 +4,27 @@
         <!-- Main content -->
         <div class="grid grid-cols-2 md:grid-cols-4 grid-rows-[auto_1fr] md:grid-rows-1 gap-2 h-full mt-2">
             <!-- Image + Title -->
-            <div class="col-span-2 md:col-span-2 row-start-1 md:order-2 flex flex-col content-center ">
-                <img src="{{ $image }}" alt="{{ $imageDescription }}" class="object-cover rounded-md">
-                <h2 class="text-4xl md:text-5xl font-semibold mt-2 text-center">{{ $title }}</h2>
+            <div class="col-span-2 md:col-span-2 row-start-1 md:order-2 flex flex-col content-center h-full">
+                <img src="{{ $image }}" alt="{{ $imageDescription }}" class="object-contain rounded-md h-full">
+                <h2 class="text-4xl md:text-5xl font-semibold mt-2 text-center h-full">{{ $title }}</h2>
             </div>
 
             <!-- Buttons -->
             <div class="h-full row-start-2 md:row-start-1 md:order-1">
                 <div class="hidden md:flex justify-start h-1/5"></div>
-                <button class="w-full h-4/5 bg-green-400 rounded-full shadow-lg hover:-translate-y-2 active:translate-y-1 active:shadow-inner transition-all duration-150 cursor-pointer"
+                <button class="w-full h-4/5 bg-green-400 rounded-full shadow-[inset_-4px_-4px_8px_rgba(0,0,0,0.3),inset_4px_4px_8px_rgba(255,255,255,0.3),0_8px_16px_rgba(0,0,0,0.4)] hover:-translate-y-2 hover:shadow-[inset_-4px_-4px_8px_rgba(0,0,0,0.3),inset_4px_4px_8px_rgba(255,255,255,0.3),0_12px_20px_rgba(0,0,0,0.5)] active:translate-y-1 active:shadow-[inset_4px_4px_12px_rgba(0,0,0,0.4),inset_-4px_-4px_8px_rgba(255,255,255,0.2)] transition-all duration-150 cursor-pointer"
                         wire:click="like"></button>
             </div>
             <div class="h-full row-start-2 md:row-start-1 md:order-3">
                 <!-- Close button -->
                 <div class="hidden md:flex justify-end h-1/5">
-                    <button class="p-2 rounded text-xl"
-                            wire:click="close">
-                        <img src="/assets/stop.svg" alt="Close" class="h-full">
-                    </button>
+                    <flux:modal.trigger name="stop-test-confirmation">
+                        <button class="p-2 rounded text-xl">
+                            <img src="/assets/stop.svg" alt="Close" class="h-full">
+                        </button>
+                    </flux:modal.trigger>
                 </div>
-                <button class="w-full h-4/5 bg-red-500 rounded-full shadow-lg hover:-translate-y-2 active:translate-y-1 active:shadow-inner transition-all duration-150 cursor-pointer"
+                <button class="w-full h-4/5 bg-red-500 rounded-full shadow-[inset_-4px_-4px_8px_rgba(0,0,0,0.3),inset_4px_4px_8px_rgba(255,255,255,0.3),0_8px_16px_rgba(0,0,0,0.4)] hover:-translate-y-2 hover:shadow-[inset_-4px_-4px_8px_rgba(0,0,0,0.3),inset_4px_4px_8px_rgba(255,255,255,0.3),0_12px_20px_rgba(0,0,0,0.5)] active:translate-y-1 active:shadow-[inset_4px_4px_12px_rgba(0,0,0,0.4),inset_-4px_-4px_8px_rgba(255,255,255,0.2)] transition-all duration-150 cursor-pointer"
                         wire:click="dislike"></button>
             </div>
         </div>
@@ -65,4 +66,24 @@
         <div  wire:key="progress-{{ $questionNumber }}"  x-data="{ progress: {{($questionNumber - 1) / $totalQuestions}} * 100 }" class="w-full bg-gray-400">
             <div class="bg-red-400 py-2" :style="`width: ${progress}%`"></div>
         </div>
+
+        <!-- Stop Test Confirmation Modal -->
+        <flux:modal name="stop-test-confirmation" class="min-w-[22rem] space-y-6">
+            <div>
+                <flux:heading size="lg">{{ __('Stop Test') }}</flux:heading>
+                <flux:subheading>
+                    <p>{{ __('Are you sure you want to stop the test?') }}</p>
+                    <p class="mt-2">{{ __('Your progress will be lost if you stop now.') }}</p>
+                </flux:subheading>
+            </div>
+
+            <div class="flex gap-2 justify-end">
+                <flux:modal.close>
+                    <flux:button variant="ghost">{{ __('Cancel') }}</flux:button>
+                </flux:modal.close>
+                <flux:modal.close>
+                    <flux:button variant="danger" wire:click="close">{{ __('Stop') }}</flux:button>
+                </flux:modal.close>
+            </div>
+        </flux:modal>
     </div>
