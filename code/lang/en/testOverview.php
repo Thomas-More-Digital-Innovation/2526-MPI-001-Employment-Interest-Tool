12e6b174718268e816a8ce0675a2af5a01b54118
Sem VB <sem.van.broekhoven@gmail.com>
2025-10-14 09:47:08 +0200

    add translations for sidebar tab

--- code/lang/en/testOverview.php
+++ code/lang/en/testOverview.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 return [
+    'tests' => 'Test Overview',
     'no_questions_found' => 'No questions found in this test.',
     'test_overview' => 'Test Overview',
     'close' => 'Back to test picker',
