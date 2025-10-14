2cb3bd760ae217ffead2b362886ab386aee1bc0c
MauritsGroen <r1037355@student.thomasmore.be>
2025-10-14 14:53:19 +0200

    Manage researchers

---
+++ code/lang/nl/manage-researchers.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'manage_researchers' => 'Beheer onderzoekers',
+    'searchResearcher' => 'Zoek onderzoeker',
+    'SearchResearcherBy' =>'Zoek alle onderzoekers op naam of gebruikersnaam',
+    'SearchResearcherByAndActive' =>'Zoek active onderzoekers op naam of gebruikersnaam',
+    'addResearcher' => 'Onderzoeker toevoegen',
+    'noResearchersFound' => 'Nog geen onderzoekers gevonden',
+    'hideInactive' => 'Verberg inactieve onderzoekers',
+    'showInactive' => 'Toon inactieve onderzoekers',
+    'noInactiveFound' => 'Geen inactieve onderzoekers gevonden',
+    'editResearcher' => 'Wijzig onderzoeker',
+    'researcherCanSignIn' => 'Onderzoeker kan inloggen',
+    'createResearcher' => 'Maak onderzoeker',
+    'deleteResearcher' => 'Verwijder onderzoeker'
+];
