7f13583c392ed589000016fd0061f0842d633009
David Maat <r1036789@student.thomasmore.be>
2025-10-16 10:42:37 +0200

    Moved image paths out of web.php and implemented caching for test images.

--- code/routes/web.php
+++ code/routes/web.php
@@ -59,72 +59,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 
-    Route::get('/profile/picture/{filename}', function ($filename) {
-        $disk = \Illuminate\Support\Facades\Storage::disk('profile_pictures');
-        $user = Auth::user();
-        switch (true) {
-            case $user->isClient(): // Client can only view the profile picture of their mentor
-            $mentor = User::find($user->mentor_id);
-            if (
-                !$mentor ||
-                $filename !== $mentor->getRawProfilePictureName()
-            ) {
-                abort(403);
-            }
-            break;
-            case $user->isMentor(): // Mentor can only view their own profile picture
-            case $user->isResearcher(): // Researcher can only view their own profile picture
-            if ($filename !== $user->getRawProfilePictureName()) {
-                abort(403);
-            }
-            break;
-            case $user->isAdmin(): // Admins can view their own profile picture and  all of the profile pictures of the mentors in their organisation
-            if ($filename === $user->getRawProfilePictureName()) {
-                break;
-            }
-            $mentor = User::where('organisation_id', $user->organisation_id)
-                ->where('profile_picture_url', $filename)
-                ->whereHas('roles', function ($q) {
-                    $q->where('role', Role::MENTOR);
-                })
-                ->first();
-            if (!$mentor || $filename !== $mentor->getRawProfilePictureName()) {
-                abort(403);
-            }
-            break;
-            case $user->isSuperAdmin(): // Superadmins can view their own profile picture and can view the profile pictures of all the admins
-            if ($filename === $user->getRawProfilePictureName()) {
-                break;
-            }
-            $admin = User::where('organisation_id', $user->organisation_id)
-                ->where('profile_picture_url', $filename)
-                ->whereHas('roles', function ($q) {
-                    $q->where('role', Role::ADMIN);
-                })
-                ->first();
-            if (!$admin || $filename !== $admin->getRawProfilePictureName()) {
-                abort(403);
-            }
-            break;
-            default:
-            abort(403);
-        }
+    Route::get('/profile/picture/{filename}', [App\Http\Controllers\ProfilePictureController::class, 'show'])->name('profile.picture');
 
-        if (!$disk->exists($filename)) {
-            abort(404);
-        }
-        $path = $disk->path($filename);
-        return response()->file($path);
-    })->name('profile.picture');
-
-    Route::get('/question/image/{filename}', function ($filename) {
-        $disk = \Illuminate\Support\Facades\Storage::disk('public');
-        if (!$disk->exists($filename)) {
-            abort(404);
-        }
-        $path = $disk->path($filename);
-        return response()->file($path);
-    })->name('question.image');
+    Route::get('/question/image/{filename}', [App\Http\Controllers\TestImageController::class, 'show'])->name('question.image');
 
     // Route to view Test Results
     Route::get('/test-results', TestResults::class)->name('client.test-result');
