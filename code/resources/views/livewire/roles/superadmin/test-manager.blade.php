8834dc9d0af8a5ee9dff0303974f690b7337b5e4
stijn-rombouts <r1036855@student.thomasmore.be>
2025-10-16 19:59:00 +0200

    Translate buttons

--- code/resources/views/livewire/roles/superadmin/test-manager.blade.php
+++ code/resources/views/livewire/roles/superadmin/test-manager.blade.php
@@ -5,8 +5,8 @@
             <div class="border bg-zinc-300/70 dark:bg-zinc-600 border-zinc-400 dark:border-zinc-400 m-2 p-2 rounded-2xl items-center flex justify-between">
                 <flux:heading size="xl">{{$test->test_name}}</flux:heading>
                 <div>
-                    <flux:button wire:click="deleteTest({{ $test->test_id }})" color="red" variant="primary">Delete</flux:button>
-                    <flux:button wire:click="loadTest({{ $test->test_id }})" color="slate" variant="primary">Edit</flux:button>
+                    <flux:button wire:click="deleteTest({{ $test->test_id }})" color="red" variant="primary" icon="trash">{{ __('actions.delete') }}</flux:button>
+                    <flux:button wire:click="loadTest({{ $test->test_id }})" color="slate" variant="primary" icon="pencil" icon:variant="outline">{{ __('actions.edit') }}</flux:button>
                 </div>
             </div>
         @endforeach
