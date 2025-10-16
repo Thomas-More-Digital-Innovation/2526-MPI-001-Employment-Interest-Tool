0f26666f18c1ab93d7dfe038dc0359cd719f4867
stijn-rombouts <r1036855@student.thomasmore.be>
2025-10-16 20:06:49 +0200

    One sidebar

--- code/resources/views/components/layouts/app/sidebar-superadmin.blade.php
+++ code/resources/views/components/layouts/app/sidebar-superadmin.blade.php
@@ -1,10 +1,6 @@
 <flux:navlist variant="outline">
-    <flux:navlist.group :heading="__('Platform')" class="grid">
+    <flux:navlist.group class="grid">
         <flux:navlist.item icon="home" :href="route('superadmin.dashboard')" :current="request()->routeIs('superadmin.dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
-    </flux:navlist.group>
-
-    <!-- SuperAdmin specific navigation -->
-    <flux:navlist.group :heading="__('SuperAdmin Specific SideBar')" class="grid">
         <flux:navlist.item icon="users" :href="route('superadmin.test.create')" :current="request()->routeIs('superadmin.test.create')" wire:navigate>{{ __('testcreation.create_test') }}</flux:navlist.item>
         <flux:navlist.item icon="users" :href="route('superadmin.test.manager')" :current="request()->routeIs('superadmin.test.manager')" wire:navigate>{{ __('testcreation.test_manager') }}</flux:navlist.item>
         <flux:navlist.item icon="users" :href="route('superadmin.interest-field-manager')" :current="request()->routeIs('superadmin.interest-field-manager')" wire:navigate>{{ __('interestfield.manager') }}</flux:navlist.item>
