725607da95b63de2c9619b1b7135439ff3b9a919
MauritsGroen <r1037355@student.thomasmore.be>
2025-10-16 13:39:09 +0200

    mail update

---
+++ code/resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+    <img src="{{ asset('LogoMPI.svg') }}" class="w-full" alt="Logo">
+</a>
+</td>
+</tr>
