4a14dcae137890efe8f98db1a45bf7355342bdd1
StefanBotnariUni <r1033310@student.thomasmore.be>
2025-10-16 15:15:42 +0200

    change favicon and title

--- code/resources/views/welcome.blade.php
+++ code/resources/views/welcome.blade.php
@@ -4,11 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ $title ?? null }}</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
