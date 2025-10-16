599dc69568d913f1451d2145b73df1d913fac3b3
StefanBotnariUni <r1033310@student.thomasmore.be>
2025-10-16 18:43:18 +0200

    fixed scroll height issue

--- code/resources/views/roles/client/dashboard.blade.php
+++ code/resources/views/roles/client/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.client>
+{{-- <x-layouts.client>
     <section class="min-h-screen flex flex-col gap-3 p-3">
         <div class="bg-zinc-600/10 dark:bg-zinc-400/10 border dark:border-zinc-300/20 rounded-2xl">
             @livewire('test.client-test-picker')
@@ -17,4 +17,21 @@
             </div>
         </div>
     </section>
+</x-layouts.client> --}}
+<x-layouts.client>
+    <section class="h-full flex flex-col">
+        <div class="flex-1 border dark:border-zinc-300/20 bg-zinc-600/10 dark:bg-zinc-400/10 m-3 rounded-2xl">
+            @livewire('test.client-test-picker')
+        </div>
+        <div class="flex-1 flex flex-col md:flex-row grow w-full">
+            <div class="border dark:border-zinc-300/20 bg-zinc-600/10 dark:bg-zinc-400/10 m-3 rounded-2xl md:w-1/2 ">
+                <flux:heading class="px-3 py-1" size="xl">{{ __('user.previous_attempts') }}</flux:heading>
+                @livewire('test.test-result-overview')
+            </div>
+            <div class="flex justify-center items-center border dark:border-zinc-300/20 bg-zinc-600/10 dark:bg-zinc-400/10 m-3 rounded-2xl md:w-1/2 ">
+                @livewire('show-mentor')
+            </div>
+        </div>
+    </section>
 </x-layouts.client>
+
