4e788a705e44422035a8cdf1cb25a8b6fd25f081
Maurits Groen <r1037355@student.thomasmore.be>
2025-10-17 22:07:14 +0200

    update first 5 best and last 5 less intrest fields (#163)

--- code/resources/views/livewire/dashboard-organisation-overview.blade.php
+++ code/resources/views/livewire/dashboard-organisation-overview.blade.php
@@ -27,11 +27,21 @@
     <div class="flex flex-wrap dark:bg-white bg-mpi dark:text-black text-white rounded-lg px-4 py-5">
         <h2 class="text-center w-full text-xl font-bold">{{__('pagesresearcher.timesIntrestfieldChosen')}}</h2>
         <div class="sm:w-full h-auto xl:w-3/4 xl:px-2 bg-white rounded-md">
-            <livewire:chart
-                :labels="$mostChosenIntrestFields->pluck('interest_field_name')->take(10)"
-                :data="$mostChosenIntrestFields->pluck('total_chosen')->take(10)"
-                class="max-w-1"
-            />
+            {{-- If more then 10 elements than give 5 best and 5 less--}}
+            @if($mostChosenIntrestFields->count()>10)
+                <livewire:chart
+                    :labels="$mostChosenIntrestFields->take(5)->merge($mostChosenIntrestFields->take(-5))->pluck('interest_field_name')"
+                    :data="$mostChosenIntrestFields->take(5)->merge($mostChosenIntrestFields->take(-5))->pluck('total_chosen')"
+                    class="max-w-1"
+                />
+                {{-- If there are not more then 10 give the normal data--}}
+            @else
+                <livewire:chart
+                    :labels="$mostChosenIntrestFields->pluck('interest_field_name')->take(10)"
+                    :data="$mostChosenIntrestFields->pluck('total_chosen')->take(10)"
+                    class="max-w-1"
+                />
+            @endif
         </div>
         <div class="sm:w-full xl:w-1/4 xl:px-2 mt-10">
             @if(!empty($mostChosenIntrestFields))
