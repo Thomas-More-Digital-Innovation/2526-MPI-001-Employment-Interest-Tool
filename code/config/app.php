f01431c4e73362e6a4921f957a33756d4a9d3aa9
stijn-rombouts <r1036855@student.thomasmore.be>
2025-10-20 13:24:59 +0200

    Fix JSON file translation

--- code/config/app.php
+++ code/config/app.php
@@ -1,7 +1,17 @@
 <?php
 
-return [
+declare(strict_types=1);
 
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Translation\TranslationServiceProvider as BaseTranslation;
+use LaravelLang\JsonFallback\TranslationServiceProvider as JsonTranslation;
+
+return [
+    'providers' => ServiceProvider::defaultProviders()->merge([
+        // other service providers
+    ])->replace([
+        BaseTranslation::class => JsonTranslation::class,
+    ])->toArray(),
     /*
     |--------------------------------------------------------------------------
     | Application Name
